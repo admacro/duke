b02042e84e72e9882c318a6c3dc7fe68dd88a647
James Ni <admacro@gmail.com>
2017-01-24 15:17:37 +0800

    more codility tasks

--- src/main/java/xyz/admacro/interview/codility/MissingInteger.java
+++ src/main/java/xyz/admacro/interview/codility/MissingInteger.java
@@ -1,9 +1,6 @@
 package xyz.admacro.interview.codility;
 
-import java.util.Arrays;
 import java.util.HashSet;
-import java.util.Set;
-
 /**
  * Write a function:
  * <p>
@@ -46,31 +43,6 @@ public class MissingInteger {
         }
 
         return num;
-//        Arrays.sort(a);
-//        Set<Integer> posInt = new HashSet<>();
-//        for (int i = 0; i < a.length; i++) {
-//            if (a[i] > 0) {
-//                posInt.add(a[i]);
-//            }
-//        }
-//        Integer[] sa = new Integer[posInt.size()];
-//        sa = posInt.toArray(sa);
-//
-//        if (sa.length == 0) return 1;
-//        if (sa.length == 1) {
-//            return sa[0] == 1 ? 2 : 1;
-//        } else {
-//            if (sa[0] > 1) {
-//                return 1;
-//            } else {
-//                for (int i = 1; i < sa.length; i++) {
-//                    if (sa[i] - sa[i - 1] > 1) {
-//                        return sa[i - 1] + 1;
-//                    }
-//                }
-//            }
-//        }
-//        return 1;
     }
 
 
@@ -85,6 +57,6 @@ public class MissingInteger {
         System.out.println(missingInteger.solution(new int[]{-2,-4,-7,-9})); // 1
         System.out.println(missingInteger.solution(new int[]{1, 3, 6, 4, 1, 2})); // 5
         System.out.println(missingInteger.solution(new int[]{-1, 3, 6, -1004, -100001, 2000000,34234,34,3,5,435,34,5,3,5342})); // 1
-        System.out.println(missingInteger.solution(new int[]{ 00001, 2000000,34234,5342})); // 1
+        System.out.println(missingInteger.solution(new int[]{ 100001, 2000000,34234,5342})); // 1
     }
 }
---
+++ src/main/java/xyz/admacro/interview/codility/PermCheck.java
@@ -0,0 +1,26 @@
+package xyz.admacro.interview.codility;
+
+import java.util.Arrays;
+
+/**
+ * Created by jni17 on 1/23/17.
+ */
+public class PermCheck {
+    public int solution(int[] A) {
+        Arrays.sort(A);
+        for (int i = 0; i < A.length; i++) {
+            if (A[i] != i + 1) {
+                return 0;
+            }
+        }
+        return 1;
+    }
+
+    public static void main(String[] args) {
+        PermCheck permCheck = new PermCheck();
+        System.out.println(permCheck.solution(new int[]{1, 3, 2}));
+        System.out.println(permCheck.solution(new int[]{4, 1, 3, 2}));
+        System.out.println(permCheck.solution(new int[]{4, 1, 3, 2, 5, 7, 6}));
+        System.out.println(permCheck.solution(new int[]{4, 1, 3, 2, 5, 7, 6, 9}));
+    }
+}
